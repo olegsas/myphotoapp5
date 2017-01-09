a9668980249dce3aeab48586b99b93dbed510f33
Oleg <o.sas@aisnovations.com>
2017-01-09 16:56:14 +0300

    Result - 1 picture I can upload!!! Then there is an error - [ng-repeat:dupes] because I try to add 2 same pictures into array key-value!!!

--- client/controllers/mainController.js
+++ client/controllers/mainController.js
@@ -3,8 +3,6 @@ angular.module('myApp').controller('mainController', ['$scope', '$http', 'Upload
         $scope.test = "Hello";
 
         $scope.pictures = [
-        'http://www.petsworld.in/blog/wp-content/uploads/2014/07/golden-puppy-with-kittens.jpg',
-        'https://s-media-cache-ak0.pinimg.com/originals/30/27/d0/3027d0b550958fa1871e0a4dd475fba6.jpg',
         'http://data.whicdn.com/images/897396/large.png'
         ];
 
@@ -15,7 +13,7 @@ angular.module('myApp').controller('mainController', ['$scope', '$http', 'Upload
                 data: {file: file},
             }).then(function(res){
                 console.log(res);
-                $scope.pictures.push('http://www.thegreatapps.com/application/upload');
+                $scope.pictures.push('http://www.thegreatapps.com/application/upload/Apps/2016/10/cat-games-free-84.png');
             });
         }
     }
